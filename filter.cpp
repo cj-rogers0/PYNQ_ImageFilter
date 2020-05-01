df949c17c54bcca726d4db4352f09ca3428a9ed2
CJ Rogers <cjrogers779@gmail.com>
2020-05-01 17:20:59 -0400

    Updated HLS Files

--- filter.cpp
+++ filter.cpp
@@ -1,20 +1,22 @@
 #include "filter.hpp"
 
-void grey_filter(AXI_STREAM &inStream, AXI_STREAM &outStream, int rows, int cols){
-#pragma HLS INTERFACE axis port=outStream
-#pragma HLS INTERFACE axis port=inStream
-#pragma HLS INTERFACE s_axilite port=rows bundle=CRTL_BUS
-#pragma HLS INTERFACE s_axilite port=cols bundle=CRTL_BUS
-#pragma HLS INTERFACE s_axilite port=return bundle=CRTL_BUS
+void grey_filter(AXI_STREAM& INPUT_STREAM, AXI_STREAM& OUTPUT_STREAM, int rows, int cols)
+{
 
-	RGB_IMAGE  img_0(rows, cols);
-	GRAY_IMAGE img_1(rows, cols);
-	RGB_IMAGE  img_2(rows, cols);
+#pragma HLS INTERFACE axis port=INPUT_STREAM
+#pragma HLS INTERFACE axis port=OUTPUT_STREAM
+#pragma HLS INTERFACe s_axilite port=rows bundle=CRTLS
+#pragma HLS INTERFACe s_axilite port=cols bundle=CRTLS
 
-	#pragma HLS dataflow
+RGB_IMAGE  img_0(rows, cols);
+GRAY_IMAGE img_1(rows, cols);
+RGB_IMAGE  img_2(rows, cols);
 
-	hls::AXIvideo2Mat(inStream, img_0);
-	hls::CvtColor<HLS_BGR2GRAY>(img_0, img_1);
-	hls::CvtColor<HLS_GRAY2RGB>(img_1, img_2);
-	hls::Mat2AXIvideo(img_1, outStream);
+#pragma HLS dataflow
+
+hls::AXIvideo2Mat(INPUT_STREAM, img_0);
+hls::CvtColor<HLS_BGR2GRAY>(img_0, img_1);
+hls::CvtColor<HLS_GRAY2RGB>(img_1, img_2);
+hls::Mat2AXIvideo(img_2, OUTPUT_STREAM);
+
 }
--- filter.hpp
+++ filter.hpp
@@ -1,15 +1,13 @@
-#include <hls_stream.h>
-#include <hls_video.h>
-#include <ap_axi_sdata.h>
+#include  "hls_video.h"
 #include <ap_fixed.h>
-
-typedef ap_axiu<32,1,1,1> image;
-typedef hls::stream<image> AXI_STREAM;
 
 #define MAX_WIDTH  2000
 #define MAX_HEIGHT 2000
 
+
+typedef hls::stream<ap_axiu<32,1,1,1> >           AXI_STREAM;
 typedef hls::Mat<MAX_HEIGHT,   MAX_WIDTH,   HLS_8UC3> RGB_IMAGE;
 typedef hls::Mat<MAX_HEIGHT,   MAX_WIDTH,   HLS_8UC1> GRAY_IMAGE;
 
-void grey_filter(AXI_STREAM &inStream, AXI_STREAM &outStream, int width, int height);
+
+void grey_filter(AXI_STREAM& INPUT_STREAM, AXI_STREAM& OUTPUT_STREAM, int rows, int cols);
